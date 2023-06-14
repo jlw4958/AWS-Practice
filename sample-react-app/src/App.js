c70082cb7e2c8e7a02e8189e0cb52614a2ec1990
Jabrecia Washington <jlw4958@g.rit.edu>
2023-06-13 20:10:41 -0400

    project setup

---
+++ sample-react-app/src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+function App() {
+  return(
+    <Router>
+      <div className="App">
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/page1">Page 1</Link></li>
+          <li><Link to="/page2">Page 2</Link></li>
+        </ul>
+        <Switch>
+          <Route path="/page1">
+            <div>Page 1</div>
+          </Route>
+          <Route path="/page2">
+            <div>Page 2</div>
+          </Route>
+          <Route path="/">
+            <div>Home</div>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
